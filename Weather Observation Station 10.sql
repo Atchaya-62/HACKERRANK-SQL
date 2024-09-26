ed4d811798a6fd60c93b575f6c164c024faa8dd4
Atchaya-62 <atchayasaran626@gmail.com>
2024-09-26 23:52:51 +0530

    Create Weather Observation Station 10.sql

---
+++ Weather Observation Station 10.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT LIKE '%A' AND CITY NOT LIKE '%E' AND CITY NOT LIKE '%I' AND CITY NOT LIKE '%O' AND CITY NOT LIKE '%U';
+
